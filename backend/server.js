ae717f8021356cb7b7d5dbe9854ab78637af788d
RasAlex <magavaj@gmail.com>
2023-04-16 18:23:22 +0300

    Basic Express Server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Server is running....');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+
+const app = express();
+
+app.get(`/`, (req, res) => {
+    // res.send(`Hello there...`)
+    res.status(200).json ({message: `Welcome to the Licenses Manager API`})
+})
+
+
+app.listen(PORT, () => console.log(`Server started on the port ${PORT}`));
