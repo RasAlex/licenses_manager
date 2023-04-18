6e4fc47fb905c7b514720b62fefedecd44ea5f3a
RasAlex <magvaj@gmail.com>
2023-04-18 16:24:55 +0300

    Error and Exeeption Handlers for User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,29 @@
-const registerUser = (req, res) => {
-    res.send(`Register User Route`)
-}
-const loginUser = (req, res) => {
-    res.send(`Login User Route`)
-}
+//Simple middleware for handling exceptions inside of async express routes and passing them to your express error handlers.
+const asyncHandler = require('express-async-handler');
 
+//@desc     Registers a new user
+//@route    /api/users
+//@access   Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    //Validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all required fields');
+
+    }
+    else {
+        return res.status(200).json({ message: `looks good ${name}` });
+    }
+})
+
+//@desc     Login user
+//@route    /api/users/login
+//@access   Public
+const loginUser = asyncHandler(async (req, res) => {
+    res.send(`Login User Route`)
+})
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-
+const {errorHandler} = require('./middleware/errorMiddleware');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get(`/`, (req, res) => {
     // res.send(`Hello there...`)
@@ -13,5 +17,6 @@ app.get(`/`, (req, res) => {
 //Routes
 app.use(`/api/users/`, require(`./routes/userRoutes`));
 app.use(`/api/schedule/`, require(`./routes/scheduleRoutes`));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on the port ${PORT}`));
