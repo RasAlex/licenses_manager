4d0bb27bc12ba65775ab3da256da220d9afbecbb
RasAlex <magvaj@gmail.com>
2023-04-19 11:19:43 +0300

    add User model and User register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 //Simple middleware for handling exceptions inside of async express routes and passing them to your express error handlers.
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
 
 //@desc     Registers a new user
 //@route    /api/users
@@ -13,8 +15,33 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Please include all required fields');
 
     }
-    else {
-        return res.status(200).json({ message: `looks good ${name}` });
+    //Check that user is already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error(`User already exists`)
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400);
+        throw new Error(`Invalid user data`);
     }
 })
 
